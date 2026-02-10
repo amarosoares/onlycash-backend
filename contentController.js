278b4c79526fe5456527d411df742bb68ac6dc94
amarosoares <139302838+amarosoares@users.noreply.github.com>
2026-02-10 23:55:28 +0000

    Update contentController.js

--- contentController.js
+++ contentController.js
@@ -1,23 +1,9 @@
-const db = require('./db');
-
-exports.getAllContent = async (req, res) => {
-  try {
-    const [rows] = await db.execute('SELECT * FROM content');
-    res.json(rows);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+exports.getAllContent = (req, res) => {
+  res.send('All content');
 };
 
-exports.createContent = async (req, res) => {
-  try {
-    const { title, description, creatorId } = req.body;
-    const [result] = await db.execute(
-      'INSERT INTO content (title, description, creatorId) VALUES (?, ?, ?)',
-      [title, description, creatorId]
-    );
-    res.status(201).json({ message: 'Content created', contentId: result.insertId });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+exports.createContent = (req, res) => {
+  res.send('Content created');
 };
+
+
