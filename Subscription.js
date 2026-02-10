e40c64ba827945c7003be0216a8f4f2485443b14
amarosoares <139302838+amarosoares@users.noreply.github.com>
2026-02-10 23:51:31 +0000

    Update Subscription.js

--- Subscription.js
+++ Subscription.js
@@ -1,14 +1,7 @@
-const db = require('./db');
+exports.createSubscription = (req, res) => {
+  res.send('Subscription created');
+};
 
-exports.createSubscription = async (req, res) => {
-  try {
-    const { userId, creatorId } = req.body;
-    const [result] = await db.execute(
-      'INSERT INTO subscriptions (userId, creatorId) VALUES (?, ?)',
-      [userId, creatorId]
-    );
-    res.status(201).json({ message: 'Subscription created', subscriptionId: result.insertId });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+exports.getAllSubscriptions = (req, res) => {
+  res.send('All subscriptions');
 };
