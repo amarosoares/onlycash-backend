91b4d03f51e1c4d938b8af052103ca04b7d53210
amarosoares <139302838+amarosoares@users.noreply.github.com>
2026-02-10 21:47:05 +0000

    Update subscriptionRoutes.js

--- subscriptionRoutes.js
+++ subscriptionRoutes.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db');
+const subscriptionController = require('./subscriptionController'); // corrigido
 
-router.post('/subscribe', (req, res) => {
-  const { user_id, creator_id, start_date, end_date, value } = req.body;
-  db.query('INSERT INTO assinaturas (usuario_id, criador_id, inicio, fim, valor) VALUES (?, ?, ?, ?, ?)',
-    [user_id, creator_id, start_date, end_date, value], (err, result) => {
-      if (err) return res.status(500).json({ error: err });
-      res.json({ message: 'Assinatura criada com sucesso!' });
-    });
-});
+router.post('/subscribe', subscriptionController.subscribeUser);
+router.get('/subscriptions/:creatorId', subscriptionController.getSubscriptions);
 
 module.exports = router;
