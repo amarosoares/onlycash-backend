35cadff6f215babfe2a90b801c414eca2f8985a4
amarosoares <139302838+amarosoares@users.noreply.github.com>
2026-02-10 23:52:22 +0000

    Update paymentController.js

--- paymentController.js
+++ paymentController.js
@@ -1,14 +1,7 @@
-const db = require('./db');
+exports.makePayment = (req, res) => {
+  res.send('Payment processed');
+};
 
-exports.createPayment = async (req, res) => {
-  try {
-    const { userId, amount, method } = req.body;
-    const [result] = await db.execute(
-      'INSERT INTO payments (userId, amount, method) VALUES (?, ?, ?)',
-      [userId, amount, method]
-    );
-    res.status(201).json({ message: 'Payment created', paymentId: result.insertId });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+exports.getPayments = (req, res) => {
+  res.send('All payments');
 };
