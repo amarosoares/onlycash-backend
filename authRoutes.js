f2106d91f99d9ef1465a261de888cfee4d37302f
amarosoares <139302838+amarosoares@users.noreply.github.com>
2026-02-10 22:04:00 +0000

    Update authRoutes.js

--- authRoutes.js
+++ authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('./authController'); // corrigido
+const authController = require('./authController');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
