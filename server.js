ecf04c47cab6929db47c59a8c93b1fbdb84404cf
amarosoares <139302838+amarosoares@users.noreply.github.com>
2026-02-11 00:42:23 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-const authRoutes = require('./authRoutes');
-const contentRoutes = require('./contentRoutes');
-const paymentRoutes = require('./paymentRoutes');
-const subscriptionRoutes = require('./subscriptionRoutes');
-
-app.use(express.json());
-
-app.use('/auth', authRoutes);
-app.use('/content', contentRoutes);
-app.use('/payments', paymentRoutes);
-app.use('/subscriptions', subscriptionRoutes);
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+app.get('/', (req, res) => {
+  res.send('OnlyCash Backend está online 🚀');
+});
