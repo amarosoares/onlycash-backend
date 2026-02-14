bb776db829b346336e13d02ba3c3478b2e8f735a
amarosoares <139302838+amarosoares@users.noreply.github.com>
2026-02-14 18:00:47 +0000

    Update authController.js

--- authController.js
+++ authController.js
@@ -1,8 +1,24 @@
 exports.register = (req, res) => {
-  res.send('User registered');
+  const { name, email, password } = req.body;
+
+  res.status(201).json({
+    success: true,
+    message: 'Usuário registrado com sucesso',
+    user: {
+      name,
+      email
+    }
+  });
 };
 
 exports.login = (req, res) => {
-  res.send('User logged in');
-};
+  const { email } = req.body;
 
+  res.status(200).json({
+    success: true,
+    message: 'Login realizado com sucesso',
+    user: {
+      email
+    }
+  });
+};
